0c9ac70254988a11c022a7cbecb3d5e9269ca854
saran <saran122005ravi@gmail.com>
2025-07-26 13:15:24 +0530

    adding the poractices

---
+++ Array.java
@@ -0,0 +1,10 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
+// This class demonstrates a simple array in Java and prints its elements.
