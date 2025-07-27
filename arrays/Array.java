fdcc20d5b5535438defa2ec5c4b4223f9a39cf3d
saranhere1 <safee000777@gmail.com>
2025-07-27 19:18:36 +0530

    restructure of learning progresses

--- arrays/Array.java
+++ arrays/Array.java
@@ -1,3 +1,4 @@
+package arrays;
 public class Array {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
--- arrays/CODE2.java
+++ arrays/CODE2.java
@@ -1,3 +1,4 @@
+package arrays;
 public class CODE2 {
         public static void main(String[]args){
             for (int num = 1; num <=5; num++){
